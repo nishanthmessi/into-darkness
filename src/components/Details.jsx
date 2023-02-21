1b99edc853b107a23e6b21e12246c055d8b6e120
unknown <nisanthrtts@gmail.com>
2023-02-21 22:26:38 +0530

    UI creation

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,14 +1,16 @@
-import Axios from "axios"
 import { useEffect, useState } from "react"
+import Axios from "axios"
 
 const Details = () => {
-  const [planetData, setPlanetData] = useState()
-  console.log(planetData)
+  const [planetData, setPlanetData] = useState("")
+  const [quickFacts, setQuickFacts] = useState("")
 
   const getPlanet =  async () => {
     const res = await Axios.get(`/api/planet/63ef2095a3a6782f2ea3a935`)
     const details = res.data.data.planetDetails
+    const facts = res.data.data
     setPlanetData(details)
+    setQuickFacts(facts)
   }
 
   useEffect(() => {
@@ -17,7 +19,7 @@ const Details = () => {
 
   return (
     <>
-    <div className='flex justify-center w-[90%] m-auto mt-10'>
+    <div className='flex flex-col justify-center w-[90%] m-auto mt-10 md:flex-row'>
       <div className='w-3/4 mx-10 flex flex-col items-center'>
         {/* <h1 className='text-xl font-bold my-2'>In Depth</h1> */}
         <p className='max-w-2xl text-xl'></p>
@@ -29,43 +31,43 @@ const Details = () => {
 
         <div className='mt-10'>
           <h1 className='text-3xl font-bold'>Potential for Life</h1>
-          <p className='max-w-2xl text-xl'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis quidem labore ab autem unde quibusdam dolorem sapiente voluptatum adipisci dolorum.</p>
+          <p className='max-w-2xl text-xl'>{planetData.potentialforLife}</p>
         </div>
 
         <div className='mt-10'>
           <h1 className='text-3xl font-bold'>Size and Distance</h1>
-          <p className='max-w-2xl text-xl'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis quidem labore ab autem unde quibusdam dolorem sapiente voluptatum adipisci dolorum.</p>
+          <p className='max-w-2xl text-xl'>{planetData.sizenDistance}</p>
         </div>
 
         <div className='mt-10'>
           <h1 className='text-3xl font-bold'>Orbit and Rotation</h1>
-          <p className='max-w-2xl text-xl'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis quidem labore ab autem unde quibusdam dolorem sapiente voluptatum adipisci dolorum.</p>
+          <p className='max-w-2xl text-xl'>{planetData.orbitnRotation}</p>
         </div>
 
         <div className='mt-10'>
           <h1 className='text-3xl font-bold'>Moons</h1>
-          <p className='max-w-2xl text-xl'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis quidem labore ab autem unde quibusdam dolorem sapiente voluptatum adipisci dolorum.</p>
+          <p className='max-w-2xl text-xl'>{planetData.moons}</p>
         </div>
       </div>
       
-      <div className='w-[30%]'>
-        <h1 className='text-3xl font-semibold mb-8'>Quick Facts</h1>
-          <div className="uppercase flex flex-col gap-4">
+      <div className='md:w-[30%] mt-12 sm:mt-0'>
+        <h1 className='text-3xl font-semibold mb-8 text-center sm:text-start'>Quick Facts</h1>
+          <div className="uppercase flex flex-col items-center gap-4 sm:items-start">
             <div>
               <h2>Distance from Sun</h2>
-              <p className="text-2xl">36384739 km</p>  
+              <p className="text-2xl">{quickFacts.distancefromSun} kms</p>  
             </div>
             <div>
               <h2>One way light time to the sun</h2>
-              <p className="text-2xl">354354545 mins</p>
+              <p className="text-2xl">{quickFacts.lightTimetoSun} mins</p>
             </div>
             <div>
-              <h2>Length of years</h2>
-              <p className="text-2xl">3435</p>
+              <h2>Length of year</h2>
+              <p className="text-2xl">{quickFacts.yearLength} Earth Days</p>
             </div>
             <div>
               <h2>Planet type</h2>
-              <p className="text-2xl">Gas Planet</p>
+              <p className="text-2xl">{quickFacts.planetType} Planet</p>
             </div> 
           </div>
       </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,17 +3,17 @@ import HeroImg from "../assets/mercurypng.png"
 const Hero = () => {
   return (
     <>
-      <div className="flex bg-gray-900 h-screen text-white">
-        <div className="flex flex-col justify-center items-center w-1/2 p-10">
-          <h1 className="text-5xl font-semibold mb-8">Welcome to Mercury</h1>
+      <div className="flex flex-col lg:flex-row bg-gray-900 h-screen text-white">
+        <div className="flex flex-col justify-center items-center md:w-1/2 p-10">
+          <h1 className="text-5xl font-semibold mb-8 text-center">Welcome to Mercury</h1>
           <p className="text-4xl text-center">Smallest planet of the solar system</p>
           <p className="text-2xl my-4">It's our first planet</p>
         </div>
-        <div className="w-1/2 h-screen flex justify-center items-center">
+        <div className="md:w-1/2 h-screen flex justify-center items-center">
           <img
             src={HeroImg}
             alt="Hero Image"
-            className="h-[70vh] w-[70wh] object-cover animate-[spin_90000ms_linear_infinite]"
+            className="h-[40vh] w-[40wh] md:h-[60vh] md:w-[60wh] object-cover animate-[spin_90000ms_linear_infinite]"
           />
         </div>
       </div>

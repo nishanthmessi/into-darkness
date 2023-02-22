9510d223e1a9b3bee4bc21c79e49f3be0047644e
unknown <nisanthrtts@gmail.com>
2023-02-22 16:48:50 +0530

    UI improvements

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -25,53 +25,53 @@ const Details = () => {
   return (
     <>
     <div className='flex flex-col justify-center w-[90%] m-auto mt-10 md:flex-row'>
-      <div className='w-3/4 mx-10 flex flex-col items-center'>
+      <div className='w-3/4 mx-10 flex flex-col items-center text-lg text-gray-700'>
         {/* <h1 className='text-xl font-bold my-2'>In Depth</h1> */}
-        <p className='max-w-2xl text-xl'></p>
+        <p className='max-w-2xl'>{planetData.description}</p>
 
         <div className='mt-10'>
           <h1 className='text-3xl font-bold'>Namesake</h1>
-          <p className='max-w-2xl text-xl'>{planetData.namesake}</p>
+          <p className='max-w-2xl'>{planetData.namesake}</p>
         </div>
 
         <div className='mt-10'>
           <h1 className='text-3xl font-bold'>Potential for Life</h1>
-          <p className='max-w-2xl text-xl'>{planetData.potentialforLife}</p>
+          <p className='max-w-2xl'>{planetData.potentialforLife}</p>
         </div>
 
         <div className='mt-10'>
           <h1 className='text-3xl font-bold'>Size and Distance</h1>
-          <p className='max-w-2xl text-xl'>{planetData.sizenDistance}</p>
+          <p className='max-w-2xl'>{planetData.sizenDistance}</p>
         </div>
 
         <div className='mt-10'>
           <h1 className='text-3xl font-bold'>Orbit and Rotation</h1>
-          <p className='max-w-2xl text-xl'>{planetData.orbitnRotation}</p>
+          <p className='max-w-2xl'>{planetData.orbitnRotation}</p>
         </div>
 
         <div className='mt-10'>
           <h1 className='text-3xl font-bold'>Moons</h1>
-          <p className='max-w-2xl text-xl'>{planetData.moons}</p>
+          <p className='max-w-2xl'>{planetData.moons}</p>
         </div>
       </div>
       
-      <div className='md:w-[30%] mt-12 sm:mt-0'>
-        <h1 className='text-3xl font-semibold mb-8 text-center sm:text-start'>Quick Facts</h1>
-          <div className="uppercase flex flex-col items-center gap-4 text-center sm:text-start sm:items-start">
+      <div className='lg:w-[30%] mt-12 lg:mt-0'>
+        <h1 className='text-3xl font-semibold mb-8 text-center lg:text-start'>Quick Facts</h1>
+          <div className="uppercase flex flex-col items-center gap-4 text-center lg:text-start md:items-start">
             <div>
-              <h2>Distance from Sun</h2>
+              <h2 className="font-semibold text-gray-600">Distance from Sun</h2>
               <p className="text-2xl">{quickFacts.distancefromSun} kms</p>  
             </div>
             <div>
-              <h2>One way light time to the sun</h2>
-              <p className="text-2xl">{quickFacts.lightTimetoSun} mins</p>
+              <h2 className="font-semibold text-gray-600">One way light time to the sun</h2>
+              <p className="text-2xl ">{quickFacts.lightTimetoSun} mins</p>
             </div>
             <div>
-              <h2>Length of year</h2>
+              <h2 className="font-semibold text-gray-600">Length of year</h2>
               <p className="text-2xl">{quickFacts.yearLength} Earth Days</p>
             </div>
             <div>
-              <h2>Planet type</h2>
+              <h2 className="font-semibold text-gray-600">Planet type</h2>
               <p className="text-2xl">{quickFacts.planetType} Planet</p>
             </div> 
           </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react"
 import HeroImg from "../assets/mercurypng.png"
 import Axios from "axios"
-import { planetDetails } from "../features/planet"
 
 const Hero = () => {
   const [planetData, setPlanetData] = useState("")
@@ -19,16 +18,16 @@ const Hero = () => {
   return (
     <>
       <div className="flex flex-col lg:flex-row bg-gray-900 h-screen text-white">
-        <div className="flex flex-col justify-center items-center md:w-1/2 p-10 gap-10">
-          <h1 className="text-5xl font-semibold text-center">Welcome to {planetData.planetName}</h1>
+        <div className="flex flex-col justify-center items-center lg:w-1/2 p-10 gap-10">
+          <h1 className="text-5xl font-semibold text-center text-white mt-10 lg:mt-0">Welcome to {planetData.planetName}</h1>
           <p className="text-4xl text-center">{planetData.subTitle}</p>
           <p className="text-2xl my-4">It's our {planetData.planetOrder} planet.</p>
         </div>
-        <div className="md:w-1/2 h-screen flex justify-center items-center">
+        <div className="lg:w-1/2 h-screen flex justify-center items-center">
           <img
             src={HeroImg}
             alt="Hero Image"
-            className="h-[30vh] w-[40wh] md:h-[60vh] md:w-[60wh] object-cover animate-[spin_90000ms_linear_infinite]"
+            className="h-[30vh] w-[40wh] md:h-[40vh] md:w-[50wh] xl:h-[55vh] xl:w-[60wh]  object-cover animate-[spin_90000ms_linear_infinite]"
           />
         </div>
       </div>
